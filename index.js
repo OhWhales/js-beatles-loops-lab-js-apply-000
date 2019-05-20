c8245f7c2a6b54b3958a9e216f385b7bff379e4c
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-20 03:30:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,9 @@
 function theBeatlesPlay(musicians, instruments)
 {
   var myArray=[];
+  for (var i=0; i < musicians.length; i++)
+  {
+    var sentence=musicians[i] + " plays " + instruments[i];
+    myArray.push(sentence);
+  }
 }
